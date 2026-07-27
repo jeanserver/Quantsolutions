4210258a22444c57640babb0d81654c131740650
jeanserver <153325408+jeanserver@users.noreply.github.com>
2026-07-27 16:21:53 -0500

    Update portfolioApi.js

--- frontend/src/api/portfolioApi.js
+++ frontend/src/api/portfolioApi.js
@@ -1,3 +1,5 @@
 import axiosClient from './axiosClient.js';
 
 export const getPortfolioSummaryRequest = () => axiosClient.get('/portfolio/summary');
+
+export const getPortfolioOverviewRequest = () => axiosClient.get('/portfolio/overview');
